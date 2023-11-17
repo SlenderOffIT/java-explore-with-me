cbbab95411794c35693b2d655045f3cf379f6804
Aleksandr Borisov <aleksandr-zm-borisov@mail.ru>
2023-11-17 15:39:24 +0800

    Создание клиента и сервиса статистики, чекстайл 2

--- ewm-service/src/test/java/practicum/AppTest.java
+++ ewm-service/src/test/java/practicum/AppTest.java
@@ -5,15 +5,15 @@ import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
 public class AppTest extends TestCase {
-    public AppTest( String testName ) {
+    public AppTest(String testName) {
         super( testName );
     }
 
     public static Test suite() {
-        return new TestSuite( AppTest.class );
+        return new TestSuite(AppTest.class);
     }
 
     public void testApp() {
-        assertTrue( true );
+        assertTrue(true);
     }
 }
